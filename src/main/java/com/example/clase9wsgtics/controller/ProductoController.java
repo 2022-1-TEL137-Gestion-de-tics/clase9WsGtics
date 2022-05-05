423bae1f2b20e9db4cc0468d93e3a0936503809a
Stuardo Lucho Hp Omen <stuardo.lucho@pucp.edu.pe>
2022-05-05 13:02:37 -0500

    listar completado

--- src/main/java/com/example/clase9wsgtics/controller/ProductoController.java
+++ src/main/java/com/example/clase9wsgtics/controller/ProductoController.java
@@ -1,13 +1,24 @@
 package com.example.clase9wsgtics.controller;
 
-import com.example.clase9gtics.repository.ProductRepository;
+import com.example.clase9wsgtics.entity.Product;
+import com.example.clase9wsgtics.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
+@CrossOrigin
 public class ProductoController {
 
     @Autowired
     ProductRepository productRepository;
 
+    @GetMapping(value = "/product", produces = MediaType.APPLICATION_JSON_VALUE + "; charset=utf-8")
+    public List<Product> listarProductos() {
+        return productRepository.findAll();
+    }
 }
